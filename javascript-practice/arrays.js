bf3d9650356b89ebdb3205cb2e53baf2dd603a9d
gk <gurpreet_kr@hotmail.com>
2026-06-11 13:35:22 +0000

    Add JavaScript arrays practice

---
+++ javascript-practice/arrays.js
@@ -0,0 +1,47 @@
+const fruits = [
+    'Apple',
+    'Banana',
+    'Orange' 
+]
+
+console.log(fruits[0])
+console.log(fruits[1])
+console.log(fruits[2])
+
+const user = [
+    'Admin',
+    'Manager',
+    'Guest'
+    ]
+
+    console.log(user[0])
+    console.log(user[1])
+    console.log(user[2])
+
+
+    const users = [
+        {username: 'Admin',
+            password: 'secret1'
+        },
+        {username: 'Manager',
+            password: 'Secret2'
+        },
+        {username: 'Guest',
+            password: 'Secret3'
+        }
+    ]
+
+    console.log(users[0].username)
+    console.log(users[0].password)
+    console.log(users[1].username)
+    console.log(users[1].password)
+    console.log(users[2].username)
+    console.log(users[2].password)  
+
+    const colours = [
+  'Red',
+  'Green',
+  'Blue'
+]
+
+console.log(colours[1])
