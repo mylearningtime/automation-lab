0f2871042d23b864f4d02bbda7dfde557f72e3e2
gk <gurpreet_kr@hotmail.com>
2026-06-11 14:08:21 +0000

    Add JavaScript oops practice

---
+++ javascript-practice/loops.js
@@ -0,0 +1,21 @@
+const users = [
+    'Admin',
+    'Manager',
+    'Guest'
+]
+for (let i=0; i<users.length; i++) 
+{    console.log(users[i])
+}
+
+const numbers = [
+    1,
+    2,
+    3,
+    4
+]
+
+for (let i=0; i<numbers.length; i++)
+{
+    console.log(numbers[i])
+}
+console.log(numbers[5]) //undefined
