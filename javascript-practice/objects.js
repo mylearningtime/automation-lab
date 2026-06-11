062c62c20add10d8b8db10c2f9786762d56f38f8
gk <gurpreet_kr@hotmail.com>
2026-06-11 13:02:19 +0000

    Javascript Objects learning

---
+++ javascript-practice/objects.js
@@ -0,0 +1,16 @@
+const user = {
+    username: 'admin',
+    password: 'secret123',
+    age: 30
+}
+console.log(user.username)
+
+const car = {
+    make: 'Toyota',
+    model: 'Corolla',
+    year: 2026
+}
+
+console.log(car.make)
+console.log(car.model)
+console.log(car.year)
